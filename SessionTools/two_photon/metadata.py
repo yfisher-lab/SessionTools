4311c0b5ef617d57cd2e6ec51b02352639780fca
markplitt <markplitt@gmail.com>
2023-01-03 17:50:31 -0800

    update metadata to read more info

--- SessionTools/two_photon/metadata.py
+++ SessionTools/two_photon/metadata.py
@@ -1,4 +1,4 @@
-"""Library for parsing metadata files from Bruker scope."""
+"""Library for parsing metadata files from Bruker scope. Modified from Deisseroth lab code"""
 
 import json
 import logging
@@ -28,14 +28,21 @@ def read(basename_input, dirname_output):
         value = element.attrib['value']
         return type_fn(value)
 
-    def indexed_value(key, index, type_fn=None, required=True):
+    def indexed_value(key, index, type_fn=None, required=True, description=False):
         element = mdata_root.find(f'.//PVStateValue[@key="{key}"]/IndexedValue[@index="{index}"]')
-        if not element:
+        if element is None:
             if required:
                 raise MetadataError('Could not find required key:index of %s:%s' % (key, index))
             return None
+        
         value = element.attrib['value']
-        return type_fn(value)
+        
+        if description:
+            return type_fn(value), element.attrib['description']
+        else:
+            return type_fn(value)
+        
+        
 
     sequences = mdata_root.findall('Sequence')
     num_sequences = len(sequences)
@@ -59,12 +66,31 @@ def read(basename_input, dirname_output):
     num_channels = len(mdata_root.find('Sequence/Frame').findall('File'))
     num_y_px = state_value('linesPerFrame', int)
     num_x_px = state_value('pixelsPerLine', int)
-
-    laser_power = indexed_value('laserPower', 0, float, required=False)
-    laser_wavelength = indexed_value('laserWavelength', 0, int, required=False)
+    
+    px_sz = {
+             'x': indexed_value('micronsPerPixel', 'XAxis', float),
+             'y': indexed_value('micronsPerPixel', 'YAxis', float),
+             'z': indexed_value('micronsPerPixel', 'ZAxis', float),
+             }
+             
+    lasers = {}
+    for i in range(3):
+        _elem = indexed_value('laserPower', i, float, required=True, description=True)
+        lasers[_elem[1]]=_elem[0]
 
     frame_period = state_value('framePeriod', float)
     optical_zoom = state_value('opticalZoom', float)
+    
+    pmt = {0: indexed_value('pmtGain', 0, float),
+           1: indexed_value('pmtGain', 1, float)}
+    
+    preamp_filter = state_value('preAmpFilter')
+    preamp_subindex = mdata_root.find('.//PVStateValue[@key="preampOffset"]/SubindexedValues[@index="0"]')
+    preampOffsets = {0: preamp_subindex.find('.//SubindexedValue[@subindex=0]').attrib['value'],
+                     1: preamp_subindex.find('.//SubindexedValue[@subindex=1]').attrib['value']}
+    
+    
+    ####
 
     voltage_root = ElementTree.parse(fname_vr_xml).getroot()
 
@@ -74,7 +100,31 @@ def read(basename_input, dirname_output):
         channel_name = signal.find('Name').text
         enabled = signal.find('Enabled').text == 'true'
         channels[channel_num] = {'name': channel_name, 'enabled': enabled}
+        
+    vr_rate = int(voltage_root.find('Experiment/Rate').text) # Hz
+    vr_acq_time = int(voltage_root.find('Experiment/AcquisitionTime').text) # ms
+    num_samp_acqd = int(voltage_root.find('SamplesAcquired').text)
 
+    
+    ####
+    markpoints_root = ElementTree.parse(fname_mp_xml).getroot()
+    
+    markpoints_dict = {}
+    for k,v in markpoints_root.attrib:
+        markpoints_dict[k] = v
+        
+    mpe = markpoints_root.find('PVMarkPointElement').attrib
+    markpoints_dict['repetitions'] = int(mpe['Repetitions'])
+    markpoints_dict['uncaging_laser'] = mpe['UncagingLaser']
+    markpoints_dict['trigger'] = mpe['TriggerSelection']
+    markpoints_dict['trigger_freq'] = mpe['TriggerFrequency']
+    markpoints_dict['trigger_count'] = int(mpe['TriggerCount'])
+    markpoints_dict['uncaging_power'] = int(mpe['UncagingLaserPower'])
+    markpoints_dict['custom_laser_power'] = [float(p)/16384.*1000 for p in mpe['CustomLaserPower'].split(',')]
+    markpoints_dict['points_list'] = [p.attrib for p in markpoints_root.findall('PVMarkPointElement/PVGalvoPointElement/Point')]
+    
+    
+    
     metadata = {
         'layout': {
             'sequences': num_sequences,
@@ -87,10 +137,8 @@ def read(basename_input, dirname_output):
             'y_px': num_y_px,
             'x_px': num_x_px
         },
-        'laser': {
-            'power': laser_power,
-            'wavelength': laser_wavelength
-        },
+        'pixel_dims': px_sz,
+        'laser_power': lasers,
         'period': frame_period,
         'optical_zoom': optical_zoom,
         'channels': channels,
