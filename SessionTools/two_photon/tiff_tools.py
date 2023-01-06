cc55d0d7525783013322a35817b16251e8e4b67e
markplitt <markplitt@gmail.com>
2023-01-06 12:13:23 -0800

    functioning tiff_tools

--- SessionTools/two_photon/tiff_tools.py
+++ SessionTools/two_photon/tiff_tools.py
@@ -1,8 +1,7 @@
 """Library for reading in TIFFs ripped from Bruker raw data."""
 import os
 import logging
-import warnings
-import h5py
+import pathlib
 
 import numpy as np
 import dask
@@ -10,11 +9,12 @@ from dask import diagnostics
 import dask.array as da
 
 from skimage.io import imread
-import utilities
+from SessionTools import utilities
 
 logger = logging.getLogger(__name__)
 
-
+class TiffToolsError(Exception):
+    """Error while processing tiff files"""
 
 def read(base, size, layout):
     """Read 2p dataset of TIFF files into a dask.Array."""
@@ -24,6 +24,7 @@ def read(base, size, layout):
     num_cycles, num_z_planes, num_ch = layout['sequences'], size['z_planes'], size['channels']
     
     def skimread(file):
+        """suppress juliandate reading errors"""
         with utilities.suppress_output(suppress_stderr=True):
             return imread(file)
         
@@ -34,11 +35,12 @@ def read(base, size, layout):
             _frame = []
             for ch in range(1,num_ch+1):
                 _frame.append(str(base) + f'_Cycle{cycle:05d}_Ch{ch}_{frame:06d}.ome.tif')
-        _filenames.append(_frame)
+            _filenames.append(_frame)
         # filenames[cycle].append(str(basename_input) + f'_Cycle{cycle+1:05d}_Ch{channel}_{frame+1:06d}.ome.tif')
         filenames.append(_filenames)    
         
-    logger.info('Found data with shape(frames, z_planes, y_pixels, x_pixels): %s', data.shape)
+    
+    logger.info('Found tiff files (channels: %i, frames: %i, z_planes: %i' % (num_cycles, num_z_planes, num_ch))
     
     def read_one_image(block_id, filenames=filenames):
         # print(block_id)
@@ -60,17 +62,8 @@ def read(base, size, layout):
     
     return data
 
-    
-    
-def unlink(fname):
-    """Helper script to delete a file."""
-    try:
-        os.remove(fname)
-    except OSError:
-        pass
 
-
-def convert_to_hdf5(data, hdf5_outname, hdf5_key = '/data'):
+def convert_to_hdf5(data, hdf5_outname, hdf5_key = '/data', overwrite=True):
     """_summary_
 
     Args:
@@ -78,12 +71,21 @@ def convert_to_hdf5(data, hdf5_outname, hdf5_key = '/data'):
         hdf5_outname (_type_): _description_
         hdf5_key (str, optional): _description_. Defaults to '/data'.
     """
+    if isinstance(hdf5_outname,str):
+        hdf5_outname = pathlib.Path(hdf5_outname)
     
+    if os.path.exists(hdf5_outname):
+        if overwrite:
+            logger.info(f'{hdf5_outname} exists. Overwriting')
+            os.remove(hdf5_outname)
+        else:
+            raise TiffToolsError(f'{hdf5_outname} exists. To overwrite set overwrite=True')
+        
     
     with diagnostics.ProgressBar():
         
         logger.info('Writing data to %s', hdf5_outname)
-        unlink(hdf5_outname)
+        # ensure parent directory exists
         os.makedirs(hdf5_outname.parent, exist_ok=True)
         data.to_hdf5(hdf5_outname, hdf5_key)
         
