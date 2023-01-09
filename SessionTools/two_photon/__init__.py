0ff131ec1855739b5fac5f55c2ba144c8e3a7ca0
markplitt <markplitt@gmail.com>
2023-01-09 13:25:03 -0800

    functioning motion correction module

--- SessionTools/two_photon/__init__.py
+++ SessionTools/two_photon/__init__.py
@@ -1,3 +1,3 @@
 from SessionTools import utilities
-from . import metadata, tiff_tools #, motion_correction
+from . import metadata, tiff_tools, motion_correction
 
--- SessionTools/two_photon/motion_correction.py
+++ SessionTools/two_photon/motion_correction.py
@@ -1,50 +1,72 @@
 
 import os
 
+from joblib import Parallel, delayed
+
+import skimage
+from skimage.registration import phase_cross_correlation
+
 import numpy as np
 from scipy.ndimage import gaussian_filter
+from scipy.ndimage import shift as spshift
 
 import SessionTools as st
 
 
-
-
-ref_channel = 0
-
-ref_intervals = np.arange(0,data.shape[1],round(data.shape[1]/10)).tolist()
-ref_frames = []
-for frame in ref_intervals:
-    for i in range(20):
-        ref_frames.append(frame+i)
-ref_img = skimage.img_as_float(data[ref_channel, ref_frames,:,:,:].mean(axis=0))
-
-
-f = h5py.File(h5name)
-data = f['/data']
-
-from skimage.registration import phase_cross_correlation
-from joblib import Parallel, delayed
-from scipy.ndimage import shift as spshift
-
-n_frames = metadata['size']['frames']
-n_zplanes = metadata['size']['z_planes']
-n_ch = metadata['size']['channels']
-shift = np.zeros((2, n_frames, n_zplanes))
-error = np.zeros((n_frames, n_zplanes))
-diffphase = np.zeros((n_frames, n_zplanes))
-
-chunk_size = int(np.minimum(4000,n_frames))
-data_chunk = data[:,:chunk_size,:,:,:]
-def compute_shift(f,z):
-    frame = skimage.img_as_float(data_chunk[ref_channel, f, z, :, :])
-    with st.utilities.suppress_output(suppress_stderr=True):
-        shift[:, f,z], error[f,z], diffphase[f,z] = phase_cross_correlation(ref_img[z, :, :], frame, upsample_factor = 10, space= 'fourier')
-    shifts = [spshift(data_chunk[ch,f,z,:,:], shift[:,f,z], order = 1, mode='reflect') for ch in range(n_ch)]
-    return (f,z, shifts)
+def make_ref_img(data, channel):
     
-# align_z_stack(0,0)
-results = Parallel(n_jobs = -1)(delayed(compute_shift)(f,z) for f in range(chunk_size) for z in range(n_zplanes))
+    ref_intervals = np.arange(0,data.shape[1],round(data.shape[1]/10)).tolist()
+    ref_frames = []
+    for frame in ref_intervals:
+        for i in range(20):
+            ref_frames.append(frame+i)
+        
+    ref_stack = data[:, ref_frames,:,:,:]
+    ref_img = skimage.img_as_float(ref_stack[channel,:,:,:,:].mean(axis=0))
+    ref_stack, _, _, _ = align_data_chunk(ref_stack, ref_img, 
+                                        ref_channel=channel, 
+                                        in_place=True)
+    return ref_stack[channel,:,:,:,:].mean(axis=0)
+    
 
-for (f,z,shifts) in results:
-    data_chunk[0,f,z,:,:] = shifts[0]
-    data_chunk[1,f,z,:,:] = shifts[1]
+def align_data_chunk(data_chunk, ref_img, ref_channel=0, in_place=True):
+    
+    n_ch = data_chunk.shape[0]
+    chunk_size, n_zplanes = data_chunk.shape[1], data_chunk.shape[2]
+    
+    # shift = np.nan*np.zeros((2, chunk_size, n_zplanes))
+    # error = np.nan*np.zeros((chunk_size, n_zplanes))
+    # diffphase = np.nan*np.zeros((chunk_size, n_zplanes))
+    
+    def compute_shift(f,z):
+        frame = skimage.img_as_float(data_chunk[ref_channel, f, z, :, :])
+        with st.utilities.suppress_output(suppress_stderr=True):
+            shift, error, diffphase = phase_cross_correlation(ref_img[z, :, :], frame, upsample_factor = 10, space='fourier')
+        shifted_data = [spshift(data_chunk[ch,f,z,:,:], shift, order = 1, mode='reflect') for ch in range(n_ch)]
+        return (f,z, shifted_data, shift, error, diffphase)
+    
+    
+    shift_results = Parallel(n_jobs=-1)(delayed(compute_shift)(f,z) for f in range(chunk_size) for z in range(n_zplanes))
+    
+    if in_place:
+        data_corr = data_chunk
+    else:
+        data_corr = 0*data_chunk
+    
+    data_corr, shifts, errors, diffphases = apply_shifts(data_corr, shift_results)
+    return data_corr, shifts, errors, diffphases
+
+def apply_shifts(data_corr, shift_results):
+    n_ch = data_corr.shape[0]
+    shifts = np.nan*np.zeros((2,*data_corr.shape[1:3]))
+    errors = np.nan*np.zeros((data_corr.shape[1], data_corr.shape[2]))
+    diffphases = errors = np.nan*np.zeros((data_corr.shape[1], data_corr.shape[2]))
+    
+    for (f, z, shifted_data, shift, error, diffphase) in shift_results:
+        shifts[:,f,z] = shift
+        errors[f,z] = error
+        diffphases[f,z] = diffphase
+        for ch in range(n_ch):
+            data_corr[ch, f, z, :, :] = shifted_data[ch]
+    return data_corr, np.array(shifts), np.array(errors), np.array(diffphases)
+    
